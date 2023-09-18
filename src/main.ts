0859da2ab87e78a41d28d26ccb2a891679520e2d
MohsenDastaran <mohsen.dastaran@gmail.com>
2023-09-18 11:20:55 +0330

    extra files removed & vuesax added

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,11 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
+import { createApp } from "vue";
+import "./style.css";
+import App from "./App.vue";
+import Vuesax from "vuesax3";
 
-createApp(App).mount('#app')
+import "vuesax3/dist/vuesax.css"; //Vuesax styles
+const app = createApp(App);
+app.use(Vuesax, {
+	// options here
+});
+app.mount("#app");
